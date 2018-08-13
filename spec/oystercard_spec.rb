edf1ac1f57349940962ca2d25718af6b0568c8bd
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-08-13 17:40:56 +0100

    Add #top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,11 +2,24 @@ require "oystercard"
 
 describe Oystercard do
 #  let(:subject) { Oystercard.new }
-  it "it responds to #balance" do
-    expect(subject).to respond_to(:balance)
-  end
+  # it "it responds to #balance" do
+  #   expect(subject).to respond_to(:balance)
+  # end
 
   it "has initial balance of 0" do
     expect(subject.balance).to eq(0)
   end
+
+  it "responds to #top_up" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  it "adds amount to #balance" do
+    card = Oystercard.new
+    amount = 20
+    card.top_up(amount)
+    expect(card.balance).to eq(amount)
+  end
+
+
 end
