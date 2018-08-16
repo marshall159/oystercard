d1a010d94746f13708421539dd2f18bad9ee90c0
Aneel Marshall <aneel.marshall@outlook.com>
2018-08-16 16:26:54 +0100

    Refactor tests

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,14 +1,13 @@
 require 'station'
 
 describe Station do
-  let(:station) { Station.new("Victoria", 1) }
+  subject { described_class.new("Victoria", 1) }
+
   it "returns #name" do
-    expect(station.name).to eq("Victoria")
+    expect(subject.name).to eq("Victoria")
   end
 
   it "returns #zone" do
-    expect(station.zone).to eq(1)
+    expect(subject.zone).to eq(1)
   end
-
-
 end
